7e132c4605c498e1059d75b8aa90ea9ac33e14b3
Timothy Mwangi <timomwa@gmail.com>
2018-04-16 22:41:23 +0300

    Update

--- hock_bot/src/main/java/org/hock_bot/core/ConfigurationI.java
+++ hock_bot/src/main/java/org/hock_bot/core/ConfigurationI.java
@@ -4,6 +4,6 @@ public interface ConfigurationI {
 	
 	public static final String API_KEY = "474326849:AAGx7g7sBcDWk_UrGenR0hdEdraG75cLVzk";
 	public static final String BOT_USERNAME = "honda_owners_club_bot";
-	public static final String CATALOG = "public";
+	public static final String CATALOG = "hock_bot";
 
 }
