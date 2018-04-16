a144e8bd92d1053c21507479c431cdbffe377a20
Timothy Mwangi <timomwa@gmail.com>
2018-04-16 19:09:19 +0300

    Update

--- hock_bot/src/main/java/org/hock_bot/rest/inbound/IncomingREST.java
+++ hock_bot/src/main/java/org/hock_bot/rest/inbound/IncomingREST.java
@@ -13,6 +13,7 @@ import javax.ws.rs.Produces;
 import org.apache.log4j.Logger;
 import org.telegram.telegrambots.api.methods.send.SendMessage;
 import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.api.objects.inlinequery.result.InlineQueryResult;
 import org.telegram.telegrambots.exceptions.TelegramApiException;
 
 @Path("/incoming")
@@ -36,17 +37,20 @@ public class IncomingREST {
 	@Produces("text/plain")
 	public Response doPost(@PathParam("token") String token, Update update) {
 		
-		logger.info("\n\n IN POST:: token--->> "+token+"\n\n");
+		logger.info("\n\n IN POST:: update --->> "+update+"\n\n");
 		
 		if (update.hasMessage() && update.getMessage().hasText()) {
-	        SendMessage message = new SendMessage() // Create a SendMessage object with mandatory fields
+			InlineQueryResult result; 
+			SendMessage message = new SendMessage() // Create a SendMessage object with mandatory fields
 	                .setChatId(update.getMessage().getChatId())
 	                .setText(update.getMessage().getText()+ (new Date()));
 	        
-	        logger.info("\n\n\t\t Incoming message  --> "+update.getMessage().getText()+"\n\n");
+	        logger.info("\n\n\t\t >>> Incoming message  --> "+update.getMessage().getText()+"\n\n");
 	        try {
 	            //execute(message); // Call method to send the message
-	        	return Response.ok(message.toString()).build();
+	        	String resp = "{ \"chat_id\": \""+update.getMessage().getChatId()+"\", \"text\": \"Text\" }";
+	        	 logger.info("\n\n\t\t << OUT  --> "+resp+"\n\n");
+	  	       return Response.ok(resp).build();
 	        }catch (Exception e) {
 	        	logger.error(e.getMessage(), e);
 	        }
