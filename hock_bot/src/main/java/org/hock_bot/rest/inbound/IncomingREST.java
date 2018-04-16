02754a52a452171725c3cb0122a6e2e4528484ab
Timothy Mwangi <timomwa@gmail.com>
2018-04-16 23:05:41 +0300

    Update

--- hock_bot/src/main/java/org/hock_bot/rest/inbound/IncomingREST.java
+++ hock_bot/src/main/java/org/hock_bot/rest/inbound/IncomingREST.java
@@ -48,7 +48,7 @@ public class IncomingREST {
 	        logger.info("\n\n\t\t >>> Incoming message  --> "+update.getMessage().getText()+"\n\n");
 	        try {
 	            //execute(message); // Call method to send the message
-	        	String resp = "{ \"method\" : \"sendMessage\", \"chat_id\": \""+update.getMessage().getChatId()+"\", \"text\": \""+message.getText()+"\" }";
+	        	String resp = "{ \"method\" : sendMessage, \"chat_id\": \""+update.getMessage().getChatId()+"\", \"text\": \""+message.getText()+"\" }";
 	        	 logger.info("\n\n\t\t << OUT  --> "+resp+"\n\n");
 	  	       return Response.ok(resp).build();
 	        }catch (Exception e) {
