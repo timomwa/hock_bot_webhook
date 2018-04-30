218697a5c0d1401b907515ad6623a99baddcafe5
Timothy Mwangi <timomwa@gmail.com>
2018-04-30 13:07:52 +0300

    Update

--- hock_bot/src/main/java/org/hock_bot/model/Chat.java
+++ hock_bot/src/main/java/org/hock_bot/model/Chat.java
@@ -6,7 +6,6 @@ import javax.persistence.JoinColumn;
 import javax.persistence.Table;
 
 import org.hock_bot.core.ConfigurationI;
-import org.telegram.telegrambots.api.objects.Message;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
